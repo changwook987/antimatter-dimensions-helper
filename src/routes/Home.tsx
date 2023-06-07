a98802ca5cc7d2215b48b81b94613743595646c6
changwook987 <changwook987@naver.com>
2023-06-07 15:30:10 +0900

    added 404.html

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
 import { FullScreen } from "../components/FullScreen";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export const Home = () => {
+    const [getSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+    useEffect(() => {
+        const path = getSearchParams.get("path");
+        if (path) {
+            navigate(path);
+        }
+    }, [getSearchParams, navigate]);
     return <FullScreen>HI</FullScreen>;
 };
